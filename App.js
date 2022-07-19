1738ee02ca46a3aa87e19e1c9b6f0c8d2f4208ba
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2022-07-19 14:40:40 -0400

    implement image converter in app

--- App.js
+++ App.js
@@ -6,14 +6,126 @@
  */
 
 import React, { useState } from 'react';
-import { Alert, Button, FlatList, PermissionsAndroid, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { Alert, Button, FlatList, Image, PermissionsAndroid, SafeAreaView, Text, View } from 'react-native';
 
-import RNBluetoothClassic, { BluetoothDevice } from 'react-native-bluetooth-classic';
+import RNBluetoothClassic from 'react-native-bluetooth-classic';
+
+import { deflateSync } from 'react-zlib-js';
+
+import CRC32 from 'crc-32';
+
+function intToBytes(int) {
+  return [
+    (int >> 24) & 0xff,
+    (int >> 16) & 0xff,
+    (int >> 8) & 0xff,
+    (int >> 0) & 0xff,
+  ];
+}
+
+function createPngChunk(fourcc, data) {
+  const result = intToBytes(data.length);
+  const checksummed = [...Buffer.from(fourcc)].concat(data);
+  return result.concat(checksummed).concat(intToBytes(CRC32.buf(checksummed)));
+}
+
+function processData(stream) {
+  let height = 0;
+  let vram = [];
+  const imageParts = [];
+
+  let i = 0;
+  while (i < stream.length) {
+    // ignore magic
+    i += 2;
+    // get command
+    const command = stream[i++];
+    // get compression
+    const compression = stream[i++];
+    // get size
+    const sizeLo = stream[i++];
+    const sizeHi = stream[i++];
+    const size = sizeLo | (sizeHi << 8);
+    // get payload
+    const payload = stream.slice(i, i + size);
+    i += size;
+    // ignore checksum and ack
+    i += 3;
+    // get status
+    const status = stream[i++];
+    // process packet
+    switch (command) {
+      case 1:
+        vram = [];
+        break;
+      case 2:
+        imageParts.push({ payload, pixels: vram.slice() });
+        height += Math.floor(vram.length / 40);
+        break;
+      case 4:
+        if (compression !== 0) {
+          let j = 0;
+          while (j < payload.length) {
+            if ((payload[j] & 0x80) !== 0) {
+              const length = (payload[j++] & 0x7f) + 2;
+              const value = payload[j++];
+              for (let k = 0; k < length; k++) vram.push(value);
+            } else {
+              const length = payload[j++] + 1;
+              for (let k = 0; k < length; k++) vram.push(payload[j++]);
+            }
+          }
+        } else {
+          vram = vram.concat(payload);
+        }
+        break;
+    }
+  }
+
+  const data = Buffer.alloc(height * 161);
+
+  let y = 0;
+  for (const { payload, pixels } of imageParts) {
+    const palette = payload[2];
+    let i = 0;
+    while (i < pixels.length) {
+      for (let x = 0; x < 160; x += 8) {
+        for (let py = 0; py < 8; py++) {
+          let lo = pixels[i++];
+          let hi = pixels[i++];
+          for (let px = 0; px < 8; px++) {
+            const index = (161 * (y + py) + x + px + 1);
+            const color = (3 - ((palette >> (2 * ((lo >> 7) + (2 * (hi >> 7))))) & 3));
+            data[index] = color;
+            lo = (lo << 1) & 0xff;
+            hi = (hi << 1) & 0xff;
+          }
+        }
+      }
+      y += 8;
+    }
+  }
+
+  let pngData = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
+  pngData = pngData.concat(createPngChunk('IHDR',
+    intToBytes(160)
+      .concat(intToBytes(height))
+      .concat([8, 3, 0, 0, 0])));
+  pngData = pngData.concat(createPngChunk('PLTE',
+    [0, 0, 0, 85, 85, 85, 170, 170, 170, 255, 255, 255]));
+  pngData = pngData.concat(createPngChunk('IDAT',
+    deflateSync(data).toJSON().data));
+  pngData = pngData.concat(createPngChunk('IEND', []));
+  const source = 'data:image/png;base64,' + Buffer.from(pngData).toString('base64');
+
+  return <Image style={{width: 160, height: 144}} source={{uri: source}} />;
+}
 
 const App = () => {
   const [bluetoothEnabled, setBluetoothEnabled] = useState(false);
   const [bondedDevices, setBondedDevices] = useState([{name: 'This is a test'}]);
   const [connectedDevice, setConnectedDevice] = useState(null);
+  const [image, setImage] = useState(null);
 
   // set up callbacks to track bluetooth state
   React.useEffect(() => {
@@ -64,19 +176,26 @@ const App = () => {
           Alert.alert('Connection failed');
         }
       }
-      device.onDataReceived(event => {
-        const bytes = Buffer.from(event.data, 'base64');
-        Alert.alert('data! [' + [...bytes] + ']');
+      let printerData = [];
+      device.onDataReceived(({ data }) => {
+        for (let i = 0; i < data.length; i += 2) {
+          if (data[i] == '!') {
+            setImage(processData(printerData));
+            printerData = [];
+          } else {
+            printerData.push(parseInt(data.substring(i, i + 2), 16));
+          }
+        }
       });
       setConnectedDevice(device);
-      await device.write('hello\n');
+      Alert.alert('i am connected');
     } catch (err) {
       Alert.alert('Could not connect: ' + err);
       console.error(err);
     }
   };
 
-  const renderBondedDevice = ({item}) => <Text onPress={requestConnect(item)}>{item.name}</Text>;
+  const renderBondedDevice = ({item}) => <Text style={{fontSize: 30}} onPress={requestConnect(item)}>{item.name}</Text>;
 
   return (
     <SafeAreaView>
@@ -88,6 +207,7 @@ const App = () => {
           onPress={checkBondedDevices}
         />
       </View>
+      {image !== null ? image : <View><Text>no image???</Text></View>}
       <FlatList
         data={bondedDevices}
         renderItem={renderBondedDevice}
--- GameBoyPrinterEmulator/GameBoyPrinterEmulator.ino
+++ GameBoyPrinterEmulator/GameBoyPrinterEmulator.ino
@@ -25,15 +25,30 @@
  *
  */
 
+#define GBP_OUTPUT_RAW_PACKETS true // by default, packets are parsed. if enabled, output will change to raw data packets for parsing and decompressing later
+#define GBP_USE_PARSE_DECOMPRESSOR false // embedded decompressor can be enabled for use with parse mode but it requires fast hardware (SAMD21, SAMD51, ESP8266, ESP32)
+
 #include <stdint.h> // uint8_t
 #include <stddef.h> // size_t
 
 #include "gameboy_printer_protocol.h"
 #include "gbp_serial_io.h"
 
+#if GBP_OUTPUT_RAW_PACKETS
+  #define GBP_FEATURE_PACKET_CAPTURE_MODE
+#else
+  #define GBP_FEATURE_PARSE_PACKET_MODE
+  #if GBP_USE_PARSE_DECOMPRESSOR
+    #define GBP_FEATURE_PARSE_PACKET_USE_DECOMPRESSOR
+  #endif
+#endif
+
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
 #include "gbp_pkt.h"
+#endif
 
 #include <SoftwareSerial.h>
+
 
 
 
@@ -77,18 +92,32 @@ SoftwareSerial bluetooth(BLUETOOTH_READ_PIN, BLUETOOTH_WRITE_PIN);
 
 // Dev Note: Gamboy camera sends data payload of 640 bytes usually
 
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
 #define GBP_BUFFER_SIZE 400
+#else
+#define GBP_BUFFER_SIZE 650
+#endif
 
 /* Serial IO */
 // This circular buffer contains a stream of raw packets from the gameboy
 uint8_t gbp_serialIO_raw_buffer[GBP_BUFFER_SIZE] = {0};
 
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
 /* Packet Buffer */
 gbp_pkt_t gbp_pktState = {GBP_REC_NONE, 0};
 uint8_t gbp_pktbuff[GBP_PKT_PAYLOAD_BUFF_SIZE_IN_BYTE] = {0};
 uint8_t gbp_pktbuffSize = 0;
+#ifdef GBP_FEATURE_PARSE_PACKET_USE_DECOMPRESSOR
+gbp_pkt_tileAcc_t tileBuff = {0};
+#endif
+#endif
 
+#ifdef GBP_FEATURE_PACKET_CAPTURE_MODE
+inline void gbp_packet_capture_loop();
+#endif
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
 inline void gbp_parse_packet_loop();
+#endif
 
 /*******************************************************************************
   Utility Functions
@@ -98,12 +127,12 @@ const char *gbpCommand_toStr(int val)
 {
   switch (val)
   {
-    case GBP_COMMAND_INIT    : return "init";
-    case GBP_COMMAND_PRINT   : return "print";
-    case GBP_COMMAND_DATA    : return "fill";
-    case GBP_COMMAND_BREAK   : return "break";
-    case GBP_COMMAND_INQUIRY : return "status";
-    default: return "unknown";
+    case GBP_COMMAND_INIT    : return "INIT";
+    case GBP_COMMAND_PRINT   : return "PRNT";
+    case GBP_COMMAND_DATA    : return "DATA";
+    case GBP_COMMAND_BREAK   : return "BREK";
+    case GBP_COMMAND_INQUIRY : return "INQY";
+    default: return "?";
   }
 }
 
@@ -164,7 +193,9 @@ void setup(void)
 #endif
 
   /* Packet Parser */
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
   gbp_pkt_init(&gbp_pktState);
+#endif
 
 #define VERSION_STRING "V3.2.1 (Copyright (C) 2022 Brian Khuu)"
 
@@ -184,7 +215,12 @@ void loop()
 {
   static uint16_t sioWaterline = 0;
 
+#ifdef GBP_FEATURE_PACKET_CAPTURE_MODE
+  gbp_packet_capture_loop();
+#endif
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
   gbp_parse_packet_loop();
+#endif
 
   // Trigger Timeout and reset the printer if byte stopped being received.
   static uint32_t last_millis = 0;
@@ -194,6 +230,8 @@ void loop()
     uint32_t elapsed_ms = curr_millis - last_millis;
     if (gbp_serial_io_timeout_handler(elapsed_ms))
     {
+      bluetooth.println("!");
+      bluetooth.flush();
       Serial.println("");
       Serial.print("// Completed ");
       Serial.print("(Memory Waterline: ");
@@ -204,7 +242,12 @@ void loop()
       Serial.flush();
       digitalWrite(LED_STATUS_PIN, LOW);
 
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
       gbp_pkt_reset(&gbp_pktState);
+#ifdef GBP_FEATURE_PARSE_PACKET_USE_DECOMPRESSOR
+      tileBuff.count = 0;
+#endif
+#endif
     }
   }
   last_millis = curr_millis;
@@ -231,21 +274,137 @@ void loop()
 
 /******************************************************************************/
 
+#ifdef GBP_FEATURE_PARSE_PACKET_MODE
+inline void gbp_parse_packet_loop(void)
+{
+  const char nibbleToCharLUT[] = "0123456789ABCDEF";
+  for (int i = 0 ; i < gbp_serial_io_dataBuff_getByteCount() ; i++)
+  {
+    if (gbp_pkt_processByte(&gbp_pktState, (const uint8_t) gbp_serial_io_dataBuff_getByte(), gbp_pktbuff, &gbp_pktbuffSize, sizeof(gbp_pktbuff)))
+    {
+      if (gbp_pktState.received == GBP_REC_GOT_PACKET)
+      {
+          digitalWrite(LED_STATUS_PIN, HIGH);
+          Serial.print((char)'{');
+          Serial.print("\"command\":\"");
+          Serial.print(gbpCommand_toStr(gbp_pktState.command));
+          Serial.print("\"");
+          if (gbp_pktState.command == GBP_COMMAND_INQUIRY)
+          {
+            // !{"command":"INQY","status":{"lowbatt":0,"jam":0,"err":0,"pkterr":0,"unproc":1,"full":0,"bsy":0,"chk_err":0}}
+            Serial.print(", \"status\":{");
+            Serial.print("\"LowBat\":");
+            Serial.print(gpb_status_bit_getbit_low_battery(gbp_pktState.status)      ? '1' : '0');
+            Serial.print(",\"ER2\":");
+            Serial.print(gpb_status_bit_getbit_other_error(gbp_pktState.status)      ? '1' : '0');
+            Serial.print(",\"ER1\":");
+            Serial.print(gpb_status_bit_getbit_paper_jam(gbp_pktState.status)        ? '1' : '0');
+            Serial.print(",\"ER0\":");
+            Serial.print(gpb_status_bit_getbit_packet_error(gbp_pktState.status)     ? '1' : '0');
+            Serial.print(",\"Untran\":");
+            Serial.print(gpb_status_bit_getbit_unprocessed_data(gbp_pktState.status) ? '1' : '0');
+            Serial.print(",\"Full\":");
+            Serial.print(gpb_status_bit_getbit_print_buffer_full(gbp_pktState.status)? '1' : '0');
+            Serial.print(",\"Busy\":");
+            Serial.print(gpb_status_bit_getbit_printer_busy(gbp_pktState.status)     ? '1' : '0');
+            Serial.print(",\"Sum\":");
+            Serial.print(gpb_status_bit_getbit_checksum_error(gbp_pktState.status)   ? '1' : '0');
+            Serial.print((char)'}');
+          }
+          if (gbp_pktState.command == GBP_COMMAND_PRINT)
+          {
+            //!{"command":"PRNT","sheets":1,"margin_upper":1,"margin_lower":3,"pallet":228,"density":64 }
+            Serial.print(", \"sheets\":");
+            Serial.print(gbp_pkt_printInstruction_num_of_sheets(gbp_pktbuff));
+            Serial.print(", \"margin_upper\":");
+            Serial.print(gbp_pkt_printInstruction_num_of_linefeed_before_print(gbp_pktbuff));
+            Serial.print(", \"margin_lower\":");
+            Serial.print(gbp_pkt_printInstruction_num_of_linefeed_after_print(gbp_pktbuff));
+            Serial.print(", \"pallet\":");
+            Serial.print(gbp_pkt_printInstruction_palette_value(gbp_pktbuff));
+            Serial.print(", \"density\":");
+            Serial.print(gbp_pkt_printInstruction_print_density(gbp_pktbuff));
+          }
+          if (gbp_pktState.command == GBP_COMMAND_DATA)
+          {
+            //!{"command":"DATA", "compressed":0, "more":0}
+#ifdef GBP_FEATURE_PARSE_PACKET_USE_DECOMPRESSOR
+            Serial.print(", \"compressed\":0"); // Already decompressed by us, so no need to do so
+#else
+            Serial.print(", \"compressed\":");
+            Serial.print(gbp_pktState.compression);
+#endif
+            Serial.print(", \"more\":");
+            Serial.print((gbp_pktState.dataLength != 0)?'1':'0');
+          }
+          Serial.println((char)'}');
+          Serial.flush();
+      }
+      else
+      {
+#ifdef GBP_FEATURE_PARSE_PACKET_USE_DECOMPRESSOR
+        // Required for more complex games with compression support
+        while (gbp_pkt_decompressor(&gbp_pktState, gbp_pktbuff, gbp_pktbuffSize, &tileBuff))
+        {
+          if (gbp_pkt_tileAccu_tileReadyCheck(&tileBuff))
+          {
+            // Got Tile
+            for (int i = 0 ; i < GBP_TILE_SIZE_IN_BYTE ; i++)
+            {
+              const uint8_t data_8bit = tileBuff.tile[i];
+              if(i == GBP_TILE_SIZE_IN_BYTE-1) {
+                Serial.print((char)nibbleToCharLUT[(data_8bit>>4)&0xF]);
+                Serial.println((char)nibbleToCharLUT[(data_8bit>>0)&0xF]); // use println on last byte to reduce serial calls
+              } else {
+                Serial.print((char)nibbleToCharLUT[(data_8bit>>4)&0xF]);
+                Serial.print((char)nibbleToCharLUT[(data_8bit>>0)&0xF]);
+                Serial.print((char)' ');
+              }
+            }
+            Serial.flush();
+          }
+        }
+#else
+        // Simplified support for gameboy camera only application
+        // Dev Note: Good for checking if everything above decompressor is working
+        if (gbp_pktbuffSize > 0)
+        {
+          // Got Tile
+          for (int i = 0 ; i < gbp_pktbuffSize ; i++)
+          {
+            const uint8_t data_8bit = gbp_pktbuff[i];
+              if(i == gbp_pktbuffSize-1) {
+                Serial.print((char)nibbleToCharLUT[(data_8bit>>4)&0xF]);
+                Serial.println((char)nibbleToCharLUT[(data_8bit>>0)&0xF]); // use println on last byte to reduce serial calls
+              } else {
+                Serial.print((char)nibbleToCharLUT[(data_8bit>>4)&0xF]);
+                Serial.print((char)nibbleToCharLUT[(data_8bit>>0)&0xF]);
+                Serial.print((char)' ');
+              }
+          }
+          Serial.flush();
+        }
+#endif
+      }
+    }
+  }
+}
+#endif
+
 static const char b64Table[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-static void sendBase64(const uint8_t *message, size_t len) {
-  const char *messageEnd = &message[len];
+static void sendBase64(size_t len) {
   uint8_t b, acc;
-  while (message != messageEnd) {
-    b = *message++;
+  while (len--) {
+    b = gbp_serial_io_dataBuff_getByte();
     bluetooth.write(b64Table[b >> 2]);
     acc = (b & 3) << 4;
-    if (message != messageEnd) {
-      b = *message++;
+    if (len--) {
+      b = gbp_serial_io_dataBuff_getByte();
       bluetooth.write(b64Table[acc | (b >> 4)]);
       acc = (b & 0x0f) << 2;
-      if (message != messageEnd) {
-        b = *message++;
+      if (len--) {
+        b = gbp_serial_io_dataBuff_getByte();
         bluetooth.write(b64Table[acc | (b >> 6)]);
         bluetooth.write(b64Table[b & 0x3f]);
       } else {
@@ -257,64 +416,55 @@ static void sendBase64(const uint8_t *message, size_t len) {
       bluetooth.write("==");
     }
   }
+  bluetooth.write('\n');
+  bluetooth.flush();
 }
 
-inline void gbp_parse_packet_loop(void)
+
+#ifdef GBP_FEATURE_PACKET_CAPTURE_MODE
+inline void gbp_packet_capture_loop()
 {
-  for (int i = 0 ; i < gbp_serial_io_dataBuff_getByteCount() ; i++)
+  /* tiles received */
+  static uint32_t byteTotal = 0;
+  static uint32_t pktTotalCount = 0;
+  static uint32_t pktByteIndex = 0;
+  static uint16_t pktDataLength = 0;
+  const size_t dataBuffCount = gbp_serial_io_dataBuff_getByteCount();
+  if (
+      ((pktByteIndex != 0)&&(dataBuffCount>0))||
+      ((pktByteIndex == 0)&&(dataBuffCount>=6))
+      )
   {
-    if (gbp_pkt_processByte(&gbp_pktState, (const uint8_t) gbp_serial_io_dataBuff_getByte(), gbp_pktbuff, &gbp_pktbuffSize, sizeof(gbp_pktbuff)))
-    {
-      if (gbp_pktState.received == GBP_REC_GOT_PACKET)
+    const char nibbleToCharLUT[] = "0123456789ABCDEF";
+    uint8_t data_8bit = 0;
+    for (int i = 0 ; i < dataBuffCount ; i++)
+    { // Display the data payload encoded in hex
+      // Start of a new packet
+      if (pktByteIndex == 0)
       {
-          digitalWrite(LED_STATUS_PIN, HIGH);
-          bluetooth.print((char)'{');
-          bluetooth.print("\"type\":\"");
-          bluetooth.print(gbpCommand_toStr(gbp_pktState.command));
-          bluetooth.print("\"");
-          if (gbp_pktState.command == GBP_COMMAND_INQUIRY) {
-            bluetooth.print(",\"status\":{");
-            bluetooth.print("\"unprocessed\":");
-            bluetooth.print(gpb_status_bit_getbit_unprocessed_data(gbp_pktState.status) ? '1' : '0');
-            bluetooth.print(",\"full\":");
-            bluetooth.print(gpb_status_bit_getbit_print_buffer_full(gbp_pktState.status)? '1' : '0');
-            bluetooth.print(",\"busy\":");
-            bluetooth.print(gpb_status_bit_getbit_printer_busy(gbp_pktState.status)     ? '1' : '0');
-            bluetooth.print(",\"checksum\":");
-            bluetooth.print(gpb_status_bit_getbit_checksum_error(gbp_pktState.status)   ? '1' : '0');
-            bluetooth.print((char)'}');
-          } else if (gbp_pktState.command == GBP_COMMAND_PRINT) {
-            bluetooth.print(",\"sheets\":");
-            bluetooth.print(gbp_pkt_printInstruction_num_of_sheets(gbp_pktbuff));
-            bluetooth.print(",\"marginUpper\":");
-            bluetooth.print(gbp_pkt_printInstruction_num_of_linefeed_before_print(gbp_pktbuff));
-            bluetooth.print(",\"marginLower\":");
-            bluetooth.print(gbp_pkt_printInstruction_num_of_linefeed_after_print(gbp_pktbuff));
-            bluetooth.print(",\"pallet\":");
-            bluetooth.print(gbp_pkt_printInstruction_palette_value(gbp_pktbuff));
-            bluetooth.print(",\"density\":");
-            bluetooth.print(gbp_pkt_printInstruction_print_density(gbp_pktbuff));
-          } else if (gbp_pktState.command == GBP_COMMAND_DATA) {
-            bluetooth.print(",\"compressed\":");
-            bluetooth.print(gbp_pktState.compression);
-            bluetooth.print(",\"more\":");
-            bluetooth.print((gbp_pktState.dataLength != 0)?'1':'0');
-          }
-          bluetooth.println((char)'}');
-          bluetooth.flush();
+        pktDataLength = gbp_serial_io_dataBuff_getByte_Peek(4);
+        pktDataLength |= (gbp_serial_io_dataBuff_getByte_Peek(5)<<8)&0xFF00;
+        digitalWrite(LED_STATUS_PIN, HIGH);
+      }
+      // Print Hex Byte
+      data_8bit = gbp_serial_io_dataBuff_getByte();
+      bluetooth.print((char)nibbleToCharLUT[(data_8bit>>4)&0xF]);
+      bluetooth.print((char)nibbleToCharLUT[(data_8bit>>0)&0xF]);
+      // Splitting packets for convenience
+      if ((pktByteIndex>5)&&(pktByteIndex>=(9+pktDataLength)))
+      {
+        digitalWrite(LED_STATUS_PIN, LOW);
+        bluetooth.println("");
+        pktByteIndex = 0;
+        pktTotalCount++;
       }
       else
       {
-        // Simplified support for gameboy camera only application
-        // Dev Note: Good for checking if everything above decompressor is working
-        if (gbp_pktbuffSize > 0)
-        {
-          bluetooth.print("{\"type\":\"data\",\"data\":\"");
-          sendBase64(gbp_pktbuff, gbp_pktbuffSize);
-          bluetooth.println("\"}");
-          bluetooth.flush();
-        }
+        pktByteIndex++; // Byte hex split counter
+        byteTotal++; // Byte total counter
       }
     }
+    bluetooth.flush();
   }
 }
+#endif
